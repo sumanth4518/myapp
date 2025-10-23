1fd4797ee9c26efd73dbd218ee4c7a8f5e9ad0cf
sumanth4518 <sumanthreddygutti@gmail.com>
2025-10-23 11:40:46 +0530

    second

--- sumanth_commerce/settings.py
+++ sumanth_commerce/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
